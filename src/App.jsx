ee484c2df30c812e4b82daf4871979edd8df8e18
UofMx2 <131321112+UofMx2@users.noreply.github.com>
2025-08-31 16:46:33 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Mike’s Peptide Ride 🚀</h1>
+      <p>If you can read this, the deploy works!</p>
+    </div>
+  )
+}
