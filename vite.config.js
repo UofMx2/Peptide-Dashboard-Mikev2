cfe1bbf714b5e65e79591b19a5b5481094688a65
UofMx2 <131321112+UofMx2@users.noreply.github.com>
2025-08-31 16:37:21 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',              // <- important for Vercel
+  base: '/',
   build: { outDir: 'dist' }
 })
