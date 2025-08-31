5218de2b91e1715fbe6b22df706ca97d74a3b2ab
UofMx2 <131321112+UofMx2@users.noreply.github.com>
2025-08-31 16:38:40 -0400

    Delete src directory

--- src/src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h1>Mike’s Peptide Ride 🚀</h1>
-      <p>If you can read this, the deploy works!</p>
-    </div>
-  )
-}
